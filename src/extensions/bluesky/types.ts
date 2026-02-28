244dd79bdfa79a43f730e0c701c5b16965441b2f
D. Scarnecchia <scarnecchia@gmail.com>
2026-02-28 09:54:46 -0500

    feat(bluesky): add BlueskyPostMetadata and BlueskyDataSource types

---
+++ src/extensions/bluesky/types.ts
@@ -0,0 +1,23 @@
+// pattern: Functional Core
+
+import type { DataSource } from "../data-source.ts";
+
+export type BlueskyPostMetadata = {
+  readonly platform: "bluesky";
+  readonly did: string;
+  readonly handle: string;
+  readonly uri: string;
+  readonly cid: string;
+  readonly rkey: string;
+  readonly reply_to?: {
+    readonly parent_uri: string;
+    readonly parent_cid: string;
+    readonly root_uri: string;
+    readonly root_cid: string;
+  };
+};
+
+export interface BlueskyDataSource extends DataSource {
+  getAccessToken(): string;
+  getRefreshToken(): string;
+}

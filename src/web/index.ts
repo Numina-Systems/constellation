8f09a7ad175dbc9dd2b87c92b46d038b5607e9ff
D. Scarnecchia <scarnecchia@gmail.com>
2026-03-01 01:44:23 -0500

    feat(web): export createFetcher from barrel

--- src/web/index.ts
+++ src/web/index.ts
@@ -2,3 +2,4 @@
 
 export type { SearchResult, SearchResponse, FetchResult, FetchCacheEntry } from "./types.ts";
 export { type SearchProvider } from "./types.ts";
+export { createFetcher } from "./fetch.ts";

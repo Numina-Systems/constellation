7048531adbd59f8f2b877d9f907b298d3ac6eb1d
D. Scarnecchia <scarnecchia@gmail.com>
2026-03-01 22:07:08 -0500

    feat(skill): add barrel exports

---
+++ src/skill/index.ts
@@ -0,0 +1,14 @@
+// pattern: Functional Core
+
+export type {
+  SkillSource,
+  SkillToolDefinition,
+  SkillMetadata,
+  SkillDefinition,
+  SkillSearchResult,
+  ParseResult,
+} from './types.ts';
+
+export type { SkillStore } from './store.ts';
+
+export { parseSkillFile } from './parser.ts';

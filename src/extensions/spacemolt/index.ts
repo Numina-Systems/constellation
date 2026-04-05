a1fb367d4e3b0418e8f32ddc4da92a6a54befcb3
D. Scarnecchia <scarnecchia@gmail.com>
2026-04-04 22:51:26 -0700

    feat: export spacemolt tool provider from barrel

--- src/extensions/spacemolt/index.ts
+++ src/extensions/spacemolt/index.ts
@@ -1,3 +1,5 @@
 export type {GameState, GameStateManager, SpaceMoltEvent} from './types.ts';
 export {createGameStateManager} from './state.ts';
 export {filterToolsByState} from './tool-filter.ts';
+export {translateMcpTool, flattenMcpContent} from './schema.ts';
+export {createSpaceMoltToolProvider, type SpaceMoltToolProviderOptions} from './tool-provider.ts';

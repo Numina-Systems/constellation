3d7dea783754f270de0f5e074523314ee807f7d4
D. Scarnecchia <scarnecchia@gmail.com>
2026-05-17 12:52:06 -0400

    feat(ingest): add barrel export for ingest module

---
+++ src/ingest/index.ts
@@ -0,0 +1,4 @@
+export { chunkDocument } from './chunker.js';
+export type { Chunk } from './chunker.js';
+export { validateIngestPath, validateFileSize } from './validate.js';
+export type { ValidationResult, FileSizeResult } from './validate.js';
